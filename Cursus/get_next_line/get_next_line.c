a05b752ec1d3704390d003f8ba6aba822fc8c680
lbengo <laurabengonava@gmail.com>
2022-11-17 21:25:19 +0100

    [gnl] new point of view

--- Cursus/get_next_line/get_next_line.c
+++ Cursus/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 12:46:34 by lbengoec          #+#    #+#             */
-/*   Updated: 2022/11/17 20:39:46 by lbengoec         ###   ########.fr       */
+/*   Updated: 2022/11/17 21:17:26 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 char *get_next_line(int fd)
 {
-	unsigned int	i;
+	int	i;
 	unsigned int	a;
 	unsigned int	j;
 	char			*line;
@@ -26,24 +26,39 @@ char *get_next_line(int fd)
 	if (fd == -1 || BUFFER_SIZE <= 0)
 		return (NULL);
 	i = 1;
-	previous_line = NULL;
-	line = NULL;
-	while (find_break(line) == 0)
+	
+	while(i >= 0)
+	{
+		line = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // control de malloc
+		i = read(fd, line, BUFFER_SIZE);
+		printf("line = %s\n", line);
+		if (i == -1)
+			return (NULL);
+		if (i == 0)
+			break ;
+		if (find_break(line) == 1)
+		{
+				// guardar el nuevo
+			break ;
+		}
+		// temporal static con malloc
+
+	}
+
+	return (0);
+}
+
+	/* while (find_break(line) == 0)
 	{
 		if (line)
 			previous_line = line;
-		line = malloc(sizeof(char) * ((BUFFER_SIZE * i) + 1));
-		read(fd, line, BUFFER_SIZE);
-		line[BUFFER_SIZE * 1] = '\0';
 		if (previous_line)
-		{
 			new_line = ft_strjoin(previous_line, line);
-			free(previous_line);
-		}
 		i++;
 		printf("line = %s\n", line);
+		printf("new_line = %s\n", new_line);
 	}
 	if (!previous_line)
 		return (line);
-	return (new_line);
-}
+	free(previous_line);
+	return (new_line); */
