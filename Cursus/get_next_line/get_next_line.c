de8ad57702744325b937bc5d2ee42a7925494edc
lbengo <laurabengonava@gmail.com>
2022-12-06 19:33:55 +0100

    [gnl] End

--- Cursus/get_next_line/get_next_line.c
+++ Cursus/get_next_line/get_next_line.c
@@ -6,93 +6,108 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 17:57:28 by lbengoec          #+#    #+#             */
-/*   Updated: 2022/12/05 12:26:09 by lbengoec         ###   ########.fr       */
+/*   Updated: 2022/12/06 19:32:34 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	int			i;
-	char		*read_line;
 	char		*line;
-	char		*temp;
 	static char	*previous_line;
+	int			i;
 
 	if (fd < 0 || BUFFER_SIZE <= 0) //fd errores lectura del txt
 		return (NULL);
-	if (previous_line && strlen_break(previous_line) >= 0) // mira si la anterior linea guardada tiene \n
-		return (cut_start_line(&previous_line));
-	read_line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if(!read_line)
-		return (NULL);
-	temp = NULL;
 	line = NULL;
-	i = 1;
+	if (previous_line && strlen_break(previous_line) >= 0) // mira si la anterior linea guardada tiene \n
+		return (cut_line(&line, &previous_line, 1));
 	if (previous_line)
+		ft_add_line(&line, &previous_line); // añade dentro de line lo que haya en previous_line
+	i = 1;
+	i = ft_read_file(fd, &line, i); // lee el file y lo añade dentro de line
+	if (i == -1)
+		return (0);
+	if (i > 0)
 	{
-		line = ft_strjoin(previous_line, line);
-		free (previous_line);
-		previous_line = NULL;
-		if (line[0] == '\0')
-		{
-			free(line);
-			line = NULL;
-		}
+		if (strlen_break(line) >= 0) //si hay salto de linea quita el resto y envia la lina sin \n
+			cut_line(&line, &previous_line, 0);
+		if (line == NULL)
+			free (previous_line);
 	}
-	while (strlen_break(line) < 0 && i > 0)
+	return (line);
+}
+
+int	ft_read_file(int fd, char **line, int i)
+{
+	char	*read_line;
+	char	*temp;
+
+	read_line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!read_line)
+		return (0);
+	temp = NULL;
+	while (strlen_break(*line) < 0 && i > 0)
 	{
 		i = read(fd, read_line, BUFFER_SIZE);
 		if (i == 0) // ha llegado al final de la lectura
-		{
-			free(read_line);
-			return (line);
-		}
+			return (ft_trama(i, &read_line, NULL));
 		if (i == -1) // si hay error en la lectura
-		{
-			free (read_line);
-			free(line);
-			return (0);
-		}
+			return (ft_trama(i, &read_line, line));
 		read_line[i] = '\0';
-		if (line)
+		if (*line)
 		{
-			temp = ft_strdup(line);
-			free(line);
+			temp = ft_strdup(*line);
+			free(*line);
 		}
-		line = ft_strjoin(temp, read_line);
+		*line = ft_strjoin(temp, read_line);
 		if (temp)
-		{
 			free(temp);
-			temp = NULL;
-		}
 	}
-	free (read_line);
-	if (strlen_break(line) >= 0) //si hay salto de linea quita el resto y envia la lina sin \n
-	{
-		if ((strlen_break(line) + 1) < (int)ft_strlen(line))
-			previous_line = ft_substr(line, strlen_break(line) + 1, ft_strlen(line));
-		temp = ft_strdup(line);
-		free(line);
-		line = ft_substr(temp, 0, strlen_break(temp) + 1);
-		free(temp);
-	}
-	if (line == NULL)
-		free (previous_line);
-	return(line);
+	return (ft_trama(i, &read_line, NULL));
 }
 
-char *cut_start_line(char **previous_line)
+int	ft_trama(int i, char **read_line, char **line)
 {
-	char	*line;
-	char	*temp;
+	free (*read_line);
+	if (line)
+		free (*line);
+	return (i);
+}
 
-	line = ft_substr(*previous_line, 0, strlen_break(*previous_line) + 1);
-	temp = ft_strdup(*previous_line);
+void	ft_add_line(char **line, char **previous_line)
+{
+	*line = ft_strjoin(*previous_line, *line);
 	free (*previous_line);
 	*previous_line = NULL;
-	*previous_line = ft_substr(temp, strlen_break(temp) + 1, ft_strlen(temp));
-	free(temp);
-	return (line);
+	if (*line[0] == '\0')
+	{
+		free(*line);
+		*line = NULL;
+	}
+}
+
+char	*cut_line(char **line, char **previous_line, int a)
+{
+	char	*temp;
+
+	if (a == 1) // añade y envía line hasta el \n de la linea anterior y recorta para la siguiente linea
+	{
+		*line = ft_substr(*previous_line, 0, strlen_break(*previous_line) + 1);
+		temp = ft_strdup(*previous_line);
+		free (*previous_line);
+		*previous_line = ft_substr(temp, strlen_break(temp) + 1, ft_strlen(temp));
+		free(temp);
+		return (*line);
+	}
+	else // recortaline es hasta \n y previous_line el resto
+	{
+		*previous_line = ft_substr(*line, strlen_break(*line) + 1, ft_strlen(*line));
+		temp = ft_strdup(*line);
+		free(*line);
+		*line = ft_substr(temp, 0, strlen_break(temp) + 1);
+		free(temp);
+	}
+	return(0);
 }
--- Cursus/get_next_line/get_next_line.h
+++ Cursus/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 08:43:04 by lbengoec          #+#    #+#             */
-/*   Updated: 2022/12/05 10:50:39 by lbengoec         ###   ########.fr       */
+/*   Updated: 2022/12/06 19:30:43 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,10 @@ int		strlen_break(char *line);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char *cut_start_line(char **previous_line);
-void cut_final_line(char *line, char **previous_line);
+void	ft_add_line(char **line, char **previous_line);
+char	*cut_line(char **line, char **previous_line, int a);
+int		ft_read_file(int fd, char **line, int i);
+int		ft_trama(int i, char **read_line, char **line);
 
 #endif
 
