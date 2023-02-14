f49a9136f06857d5da7b4de2f3f11a140629bb4f
lbengo <laurabengonava@gmail.com>
2023-02-14 20:08:50 +0100

    [so_long] Errors missing NULL and .xpm error

--- Cursus/so_long_new/src/check_error.c
+++ Cursus/so_long_new/src/check_error.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 08:40:21 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/02/14 14:44:45 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/02/14 20:08:03 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 // Check error map
 static int	check_map(char **map)
 {
-	unsigned int i;
-	int len;
+	unsigned int	i;
+	int				len;
 
 	i = 0;
 	len = strlen_line(map[0]);
@@ -24,79 +24,106 @@ static int	check_map(char **map)
 	{
 		if (len != strlen_line(map[i]))
 		{
-			printf("Error: The map must be rectangular.\n");
-			return(2);
+			printf("Error: The map must be rectangular.\n\n");
+			return (2);
 		}
 		if (map[i][0] != '1' || map[i][len - 1] != '1')
 		{
-			printf("Error: The map must be surrounded by walls.\n");
-			return(2);
+			printf("Error: The map must be surrounded by walls.\n\n");
+			return (2);
 		}
 		i++;
 	}
-	return(0);
+	return (0);
+}
+
+static int	send_numb_caract(char **map, char c)
+{
+	unsigned int	i;
+	unsigned int	a;
+	unsigned int	count;
+
+	i = 0;
+	count = 0;
+	while (map[i] != NULL)
+	{
+		a = 0;
+		while (map[i][a] != '\0')
+		{
+			if (map[i][a] == c)
+				count++;
+			a++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+// Check error caracter
+static int	check_caract(char **map, char c)
+{
+	unsigned int	caract;
+
+	caract = send_numb_caract(map, c);
+	if (caract == 0)
+	{
+		printf("Error: Component '%c'is required.\n\n", c);
+		return (2);
+	}
+	if (caract > 1)
+	{
+		printf("Error: There is more than one component'%c'.\n\n", c);
+		return (2);
+	}
+	return (0);
 }
 
 // Check error path
 static int	check_path(char **map)
 {
-	unsigned int i;
-	unsigned int a;
+	unsigned int	i;
+	unsigned int	a;
 
 	check_end(map, ft_find_p(map, 'x'), ft_find_p(map, 'y'));
 	i = 0;
-	a = 0;
-	while (map[i][a] != '\0')
+	while (map[i] != NULL)
 	{
-		while(map[i][a] != '\0')
+		a = 0;
+		while (map[i][a] != '\0')
 		{
-			printf("%c", map[i][a]);
 			if (map[i][a] == 'E')
 			{
-				printf("Error: The character would never reach the exit.\n");
+				printf("Error: The character would never reach the exit.\n\n");
 				return (2);
 			}
 			if (map[i][a] == 'C')
 			{
-				printf("Error: The character would never reach the object.\n");
+				printf("Error: The character would never reach the object.\n\n");
 				return (2);
 			}
 			a++;
 		}
-		printf("eeee %c", map[i][a]);
 		i++;
 	}
-	return(0);
+	return (0);
 }
 
 int	check_error(t_program *program)
 {
-	//check_end(temp, ft_find_p(temp, 'x'), ft_find_p(temp, 'y'));
-	char **new_map;
+	char	**new_map;
 
-	if (program -> map[0] == NULL)
-		printf("Error\n");
+	if (program -> map[0] == NULL) // NO FUNCIONA
+		return (2);
 	if (check_map(program -> map) == 2)
-		printf("Error\n");
-	//mirar si hay dobleso no hay alguno
-	//printf("Error: A starting position 'P' is required.\n");
+		return (2);
+	if (check_caract(program -> map, 'P') == 2 || check_caract
+		(program -> map, 'E') == 2 || check_caract(program -> map, 'C') == 2)
+		return (2);
 	new_map = duplicate_map(program -> map);
 	if (check_path(new_map) == 2)
-		printf("Error\n");
-
-
-	printf("map[0] = %s\n", new_map[0]);
-	printf("map[1] = %s\n", new_map[1]);
-	printf("map[2] = %s\n", new_map[2]);
-	printf("map[3] = %s\n", new_map[3]);
-	printf("map[4] = %s\n\n", new_map[4]);
-
+		return (2);
 	ft_free(new_map);
-	//- No existe algun objeto
-	//- Más de un objeto
+	return (0);
+}
 	//- NULL
 	//- .xpm sale error
-	return(2);
-}
-
-
--- Cursus/so_long_new/src/so_long.c
+++ Cursus/so_long_new/src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:41:32 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/02/14 14:38:19 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/02/14 20:02:48 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,28 +38,6 @@ int	main(int argc, char *argv[])
 		program.map = ft_matrix(program.map_width, argv[1]); // matriz del mapa
 		if (check_error(&program) == 2)
 		//	return (0);
-
-		printf("map[0] = %s\n", program.map[0]);
-		printf("map[1] = %s\n", program.map[1]);
-		printf("map[2] = %s\n", program.map[2]);
-		printf("map[3] = %s\n", program.map[3]);
-		printf("map[4] = %s\n", program.map[4]);
-/* 		printf("map[5] = %s\n", program.map[5]);
-		printf("map[6] = %s\n", program.map[6]);
-		printf("map[7] = %s\n", program.map[7]);
-		printf("map[8] = %s\n", program.map[8]);
-		printf("map[9] = %s\n", program.map[9]);
-		printf("map[10] = %s\n", program.map[10]);
-		printf("map[11] = %s\n", program.map[11]);
-		printf("map[12] = %s\n", program.map[12]);
-		printf("map[13] = %s\n", program.map[13]);
-		printf("map[14] = %s\n", program.map[14]);
-		printf("map[15] = %s\n", program.map[15]);
-		printf("map[16] = %s\n", program.map[16]);
-		printf("map[17] = %s\n", program.map[17]);
-		printf("map[18] = %s\n", program.map[18]);
-		printf("map[18] = %s\n", program.map[19]); */
-
 		ft_free(program.map);
 	}
 	return (0);
