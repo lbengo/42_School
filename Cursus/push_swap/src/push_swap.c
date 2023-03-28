5ac5d5bb8f74d50ab835dde7c0f3306ff9afe4e7
lbengo <laurabengonava@gmail.com>
2023-03-28 14:28:25 +0200

    [push_swap] reverse_rotate_a done

--- Cursus/push_swap/src/push_swap.c
+++ Cursus/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:45:32 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/03/28 13:46:45 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/03/28 14:21:30 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 //Si el primer número es mayor que todos los de la lista lo cambia a la lista B. Ej: 431(lista a). -> 31(lista a) 4(lista b)
 //Si no, lo rota al último de la misma lista. Ej: 143. -> 431
 
+/* rotate a - desplaza hacia arriba todos los elementos del stack a una posición,
+de forma que el primer elemento se convierte en el último */
 void rotate_a(t_lst **lst)
 {
 	t_lst *curr;
@@ -25,10 +27,8 @@ void rotate_a(t_lst **lst)
 	(*lst) = curr;
 }
 
-/* push b - toma el primer elemento del stack a y lo pone encima del stack b.
-No hace nada si a está vacío */
-
-static void push_b(t_lst **lst_a, t_lst **lst_b)
+/* push b - toma el primer elemento del stack a y lo pone encima del stack b. */
+void push_b(t_lst **lst_a, t_lst **lst_b)
 {
 	t_lst *last_nbr;
 
@@ -37,8 +37,20 @@ static void push_b(t_lst **lst_a, t_lst **lst_b)
 	if (lst_b)
 	last_nbr->next = *lst_b;
 	*lst_b = last_nbr;
+}
 
+/* reverse rotate a - desplaza hacia abajo todos los elementos del stack a una
+posición, de forma que el último elemento se convierte en el primero. */
+void reverse_rotate_a(t_lst **lst)
+{
+	t_lst *penultimate;
+	t_lst *curr;
 
+	penultimate = ft_lstpenultimate(*lst);
+	curr = ft_lstlast(*lst);
+	curr->next = *lst;
+	*lst = curr;
+	penultimate->next = NULL;
 }
 
 int main (int argc, char *argv[])
@@ -66,7 +78,7 @@ int main (int argc, char *argv[])
 	}
 
 	//rotación y comprobación de lista
-	push_b(&lst_a, &lst_b);
+	reverse_rotate_a(&lst_a);
 
 	t_lst *curr_a;
 
@@ -76,14 +88,14 @@ int main (int argc, char *argv[])
 		curr_a = curr_a->next;
 	}
 
-	t_lst *curr_b;
+	/* t_lst *curr_b;
 
 	curr_b = lst_b;
 	while (curr_b) {
 		printf("bbbbb = %d\n", curr_b->content);
 		curr_b = curr_b->next;
 	}
-
+ */
 
 
 
