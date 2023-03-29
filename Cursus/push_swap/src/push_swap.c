7583bb509ca44d8f2e232059ff9fb2652f4b8173
Laura Bengoechea <laurabengoechea@MacBook-Pro-de-Laura-2.local>
2023-03-29 07:13:28 +0200

    [push_swap] Create funtion swap_a

--- Cursus/push_swap/src/push_swap.c
+++ Cursus/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: laurabengoechea <laurabengoechea@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:45:32 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/03/28 20:39:48 by laurabengoe      ###   ########.fr       */
+/*   Updated: 2023/03/29 07:12:03 by laurabengoe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,28 @@
 //Si el primer número es mayor que todos los de la lista lo cambia a la lista B. Ej: 431(lista a). -> 31(lista a) 4(lista b)
 //Si no, lo rota al último de la misma lista. Ej: 143. -> 431
 
+/* sa : swap a - intercambia los dos primeros elementos encima del stack a. No hace
+nada si hay uno o menos elementos */
+void swap_a(t_lst **lst)
+{
+	t_lst *second_nbr;
+	
+	second_nbr = (*lst)->next;
+	(*lst)->next = second_nbr->next;
+	second_nbr->next = *lst;
+	*lst = second_nbr;
+}
+
 /* rotate a - desplaza hacia arriba todos los elementos del stack a una posición,
 de forma que el primer elemento se convierte en el último */
 void rotate_a(t_lst **lst)
 {
-	t_lst *curr;
+	t_lst *second_nbr;
 
-	curr = (*lst)->next;
+	second_nbr = (*lst)->next;
 	ft_lstlast(*lst)->next = *lst;
 	(*lst)->next = NULL;
-	(*lst) = curr;
+	(*lst) = second_nbr;
 }
 
 /* push b - toma el primer elemento del stack a y lo pone encima del stack b. */
@@ -77,9 +89,7 @@ int main (int argc, char *argv[])
 	}
 
 	//rotación y comprobación de lista
-	push_b(&lst_a, &lst_b);
-	push_b(&lst_a, &lst_b);
-
+	swap_a(&lst_a);
 
 	t_lst *curr_a;
 
@@ -89,13 +99,13 @@ int main (int argc, char *argv[])
 		curr_a = curr_a->next;
 	}
 
-	t_lst *curr_b;
+	/* t_lst *curr_b;
 
 	curr_b = lst_b;
 	while (curr_b) {
 		printf("bbbbb = %d\n", curr_b->content);
 		curr_b = curr_b->next;
-	}
+	} */
 
 
 
