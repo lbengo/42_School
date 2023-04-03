94996206331b2486469898d0b390ad964428214b
lbengo <laurabengonava@gmail.com>
2023-04-03 10:17:51 +0200

    [push_swap] Sorting of a small list, completed

--- Cursus/push_swap/src/push_swap.c
+++ Cursus/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:45:32 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/03/30 11:59:32 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/04/03 10:16:26 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 //Si el primer número es mayor que todos los de la lista lo cambia a la lista B. Ej: 431(lista a). -> 31(lista a) 4(lista b)
 //Si no, lo rota al último de la misma lista. Ej: 143 -> 431
 
-static void	find_move_top(t_lst **lst_a)
+/* static void	find_move_top(t_lst **lst_a)
 {
 	t_lst *curr;
 	int	len;
@@ -40,8 +40,51 @@ static void	find_move_top(t_lst **lst_a)
 
 static void	find_move_b(t_lst **lst_a, t_lst **lst_b)
 {
-	
+
+} */
+
+static int	check_order(t_lst **lst)
+{
+	t_lst *curr;
+	t_lst *next;
+
+	curr = *lst;
+	next = (*lst)->next;
+	while(next != NULL)
+	{
+		if(curr->content > next->content)
+			return(1);
+		curr = curr->next;
+		next = next->next;
+	}
+	return(0);
 }
+
+// 3 numbers
+
+static void	small_nbr(t_lst **lst)
+{
+	t_lst	*first;
+	t_lst	*second;
+	t_lst	*third;
+
+	while(check_order(lst) == 1)
+	{
+		first = (*lst);
+		second = (*lst)->next;
+		third = second->next;
+		if (first->content > second->content && first->content < third->content)
+			swap_a(lst);
+		if (first->content > second->content && first->content > third->content)
+			rotate_a(lst);
+		if (first->content < second->content && first->content > third->content)
+			reverse_rotate_a(lst);
+		if (first->content < second->content && first->content < third->content)
+			swap_a(lst);
+	}
+}
+
+
 
 int	main(int argc, char *argv[])
 {
@@ -56,8 +99,9 @@ int	main(int argc, char *argv[])
 	lst_a = add_to_lst(--argc, ++argv);
 	lst_b = NULL;
 	check_duplicate(lst_a);
-	find_move_top(&lst_a);
-	find_move_b(&lst_a, &lst_b);
+	small_nbr(&lst_a);
+	//find_move_top(&lst_a);
+	//find_move_b(&lst_a, &lst_b);
 
 	//Comprobación de lista
 	t_lst *curr;
@@ -66,7 +110,6 @@ int	main(int argc, char *argv[])
 	while (curr != NULL)
 	{
 		printf("lista = %d\n", curr->content);
-		printf("top = %d\n", curr->move_top);
 		curr = curr -> next;
 	}
 
