8b108554f7780666bc6dbdb17568282e9dd4e3b4
lbengo <laurabengonava@gmail.com>
2023-04-11 20:02:24 +0200

    [push_swap] Starting with algorithm of 5

--- Cursus/push_swap/src/algorithm/small_algorithm.c
+++ Cursus/push_swap/src/algorithm/small_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:24:13 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/04/11 17:39:07 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:52:36 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,78 @@ void	three_nbr(t_lst **lst)
 	}
 }
 
+static int	find_space(t_lst *lst_a, t_lst *lst_b)
+{
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_lstsize(lst_a);
+	while (lst_a != NULL)
+	{
+		if (lst_b->content < lst_a->content)
+			break;
+		i++;
+		lst_a = lst_a->next;
+	}
+	if (i > len/2)
+		i = i - len;
+	return (i);
+}
+
+static void	order_lst_a(t_lst **lst)
+{
+	t_lst	*curr;
+	int		len;
+	int		i;
+
+	i = 0;
+	curr = *lst;
+	len = ft_lstsize(curr);
+	while (curr != NULL)
+	{
+		if (curr->content == (check_max_min(curr, curr->content, 'M')))
+			break;
+		i++;
+		curr = curr->next;
+	}
+	while (check_order(lst, 'a') == 1)
+	{
+		if (i >= len/2)
+			reverse_rotate_a(lst);
+		else
+			rotate_a(lst);
+	}
+}
+
+static void	send_to_a(t_lst **lst_a, t_lst **lst_b)
+{
+	int i;
+
+	i = find_space (*lst_a, *lst_b);
+	while (i != 0)
+	{
+		if (i > 0)
+		{
+			rotate_a(lst_a);
+			i--;
+		}
+		else if (i < 0)
+		{
+			reverse_rotate_a(lst_a);
+			i++;
+		}
+	}
+	push_a(lst_a, lst_b);
+	order_lst_a(lst_a);
+}
+
 // Algorithm for 5 numbers
 void	five_nbr(t_lst **lst_a, t_lst **lst_b)
 {
-	lst_a = NULL;
-	lst_b = NULL;
-	printf("start\n");
+	push_b(lst_a, lst_b);
+	push_b(lst_a, lst_b);
+	three_nbr(lst_a);
+	send_to_a(lst_a, lst_b);
+	send_to_a(lst_a, lst_b);
 }
