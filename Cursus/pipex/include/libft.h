c6fddf50b676ba864feb8a584caa251b9149f569
lbengo <laurabengonava@gmail.com>
2023-05-12 13:08:37 +0200

    [pipex] Env done

--- Cursus/pipex/include/libft.h
+++ Cursus/pipex/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 11:34:28 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/04/19 17:15:34 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/05/12 11:06:00 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
--- Cursus/pipex/src/pipex.c
+++ Cursus/pipex/src/pipex.c
@@ -6,28 +6,11 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:00:37 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/05/12 10:20:11 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/05/12 13:07:41 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-/* void	ft_make_command(char *argv)
-{
-	int val;
-	char *command;
-	char **args;
-
-
-	command = ft_strjoin("/bin/", argv);
-	args = ft_split(command, ' ');
-	val = execve(args[0], args, NULL);
-	if (val == -1)
-		perror("Error");
-	//printf("aaaaa");
-	free (command); // no se libera adecuadamente
-	ft_free(args); // no se libera adecuadamente
-} */
 
 void	ft_free(char **str)
 {
@@ -42,26 +25,74 @@ void	ft_free(char **str)
 	free(str);
 }
 
-void	ft_make_command(char *argv)
+char	*ft_find_path(char **env)
 {
-	int val;
-	char *command;
-	char **args;
+	char	*path;
+	int		i;
 
-	command = ft_strjoin("/bin/", argv);
-	args = ft_split(command, ' ');
-	val = execve(args[0], args, NULL);
-	if (val == -1)
-		perror("Error");
-		
-	free (command); // no se libera adecuadamente
-	ft_free(args); // no se libera adecuadamente
+	i = 0;
+	path = NULL;
+	while (env[i])
+	{
+		if (ft_strstr(env[i], "PATH") != NULL)
+			return(ft_strstr(env[i], "PATH"));
+		i++;
+	}
+	return(path);
+}
+
+char	**ft_separate_path(char **env)
+{
+	char	*all_path;
+	char	*temp;
+	char	**path;
+	int		i;
+
+	all_path = ft_find_path(env);
+	path = ft_split(all_path, ':');
+	i = 0;
+	while (path[i])
+	{
+		temp = path[i];
+		free(path[i]);
+		path[i] = ft_strjoin(temp, "/");
+		i++;
+	}
+	return(path);
 }
 
 #define READ_FD 0
 #define WRITE_FD 1
 
-int	ft_pipex(int argc, char *argv[]) // TODO: habría que hacer un bucle para que vaya pasando comandos x veces
+int	ft_make_command(char *argv, char **env)
+{
+	int i;
+	int val;
+	char *command;
+	char **path;
+	char **args;
+
+	path = ft_separate_path(env);
+	i = 0;
+	while(path[i])
+	{
+		command = ft_strjoin(path[i], argv);
+		args = ft_split(command, ' ');
+		val = execve(args[0], args, NULL);
+		i++;
+	}
+	if (val == -1)
+	{
+		printf("aaaaa\n");
+		free(command);
+		ft_free(args);
+		perror("Error");
+		exit (0);
+	}
+	return (0);
+}
+
+int	ft_pipex(int argc, char *argv[], char **env) // TODO: habría que hacer un bucle para que vaya pasando comandos x veces
 {
 	pid_t	pid;
 	int		fd1[2];
@@ -80,43 +111,33 @@ int	ft_pipex(int argc, char *argv[]) // TODO: habría que hacer un bucle para qu
 		dup2(fd1[WRITE_FD], STDOUT_FILENO);
 		close(fd1[WRITE_FD]);
 
-		ft_make_command(argv[2]);
+		ft_make_command(argv[2], env);
 	}
 	else // padre
-	{
 		close(fd1[WRITE_FD]);
-
+	pid = fork();
+	if (!pid)
+	{
 		dup2(fd1[READ_FD], STDIN_FILENO);
 		close(fd1[READ_FD]);
 
-		ft_make_command(argv[3]);
+		ft_make_command(argv[3], env);
 	}
+	else
+		close(fd1[0]);
 	return (0);
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char **env)
 {
 	if (argc == 5)
 	{
 		if (check_file(argv) == 1)
 			return (1);
-		if (ft_pipex(argc, argv) == 1)
+		if (ft_pipex(argc, argv, env) == 1)
 			return (1);
 	}
 	else
 		printf("Error\nArgumentos esperados: 4. Entregados: %d.\n", (argc - 1));
-	//exit(0);
 	return (0);
 }
-
-/* int	main(int argc, char *argv[])
-{
-	argv = NULL;
-	if (argc < 5)
-	{
-		printf("Error\nArgumentos mínimos esperados: 4. Entregados: %d.\n", (argc - 1));
-		return (1);
-	}
-	ft_pipex(argc, argv);
-	return (0);
-} */
