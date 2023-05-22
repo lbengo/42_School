66635726daae013c0722e5ee34d7ad768085369d
lbengo <laurabengonava@gmail.com>
2023-05-22 10:20:30 +0200

    [pipex] Makes multiple commands, a while is needed to make it automated

--- Cursus/pipex/src_bonus/pipex.c
+++ Cursus/pipex/src_bonus/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lbengoec <lbengoec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:00:37 by lbengoec          #+#    #+#             */
-/*   Updated: 2023/05/22 10:08:04 by lbengoec         ###   ########.fr       */
+/*   Updated: 2023/05/22 10:18:59 by lbengoec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,18 +44,47 @@ int	file_in(char *argv[], char **env)
 	return (fd[READ_FD]);
 }
 
-int	file_out(char *argv[], char **env, int fd[2])
+int	make_cmds(char *argv[], char **env, int fdin)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	pipe(fd);
+	pid = fork(); // último
+	if (pid == -1) // error
+		return (1);
+	if (pid == 0)
+	{
+		dup2(fdin, STDIN_FILENO);
+		close(fdin);
+
+		dup2(fd[WRITE_FD], STDOUT_FILENO);
+		close(fd[WRITE_FD]);
+
+		exec_cmd(argv[2], env);
+		return (1);
+	}
+	else // padre
+	{
+		wait(NULL);
+		close(fd[WRITE_FD]);
+	}
+
+	return (fd[READ_FD]);
+}
+
+int	file_out(char *argv[], char **env, int fdin)
 {
 	int		file;
 
-	dup2(fd[READ_FD], STDIN_FILENO);
-	close(fd[READ_FD]);
+	dup2(fdin, STDIN_FILENO);
+	close(fdin);
 
-	file = open(argv[3], O_CREAT | O_TRUNC | O_RDWR , 0644);
+	file = open(argv[4], O_CREAT | O_TRUNC | O_RDWR , 0644);
 	dup2(file, STDOUT_FILENO);
 	close(file);
 
-	exec_cmd(argv[2], env);
+	exec_cmd(argv[3], env);
 	return (1);
 }
 
@@ -66,8 +95,8 @@ int	ft_pipex(char *argv[], char **env)
 	int	fd;
 
 	fd = file_in(argv, env);
-	if (file_out(argv, env, &fd) == 1)
-		return (1);
+	fd = make_cmds(argv, env, fd);
+	file_out(argv, env, fd);
 	return (0);
 }
 
@@ -84,34 +113,3 @@ int	main(int argc, char *argv[], char **env)
 		error_message("Error: Argumentos mínimos esperados: 4\n");
 	return (0);
 }
-
-/* int	ft_make_multiple_commands(char *argv[], char **env, int fdin)
-{
-	pid_t	pid;
-	int		fd[2];
-
-	pipe(fd);
-	pid = fork(); // último
-	if (pid == -1) // error
-		return (1);
-	if (pid == 0)
-	{
-		dup2(fdin, STDIN_FILENO);
-		close(fdin);
-
-		printf("argv = %s\n", argv[2]);
-
-		dup2(fd[WRITE_FD], STDOUT_FILENO);
-		close(fd[WRITE_FD]);
-
-		ft_execute_command(argv[2], env);
-		return (1);
-	}
-	else // padre
-	{
-		wait(NULL);
-		close(fd[WRITE_FD]);
-	}
-
-	return (STDOUT_FILENO);
-} */
